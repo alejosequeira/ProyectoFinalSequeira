7a88a642a83bd15a911bbff4017589951fc28970
alejosequeira <sequeiraar@gmail.com>
2023-09-01 13:58:57 -0300

    PreEntrega1-Sequeira

--- generator.js
+++ generator.js
@@ -1,10 +1,31 @@
-const inp= prompt()
+let numCaracteres= parseInt(prompt("La contraseña debe tener entre 8 y 16 caracteres, ingresa un valor."))
+let numeroValido= validarLargo(numCaracteres)
 const password=[]
-function randomPass(inp){
-for(let i=1;i<=inp;i++){
-password.push(parseInt((i*Math.random())%9))
+
+function validarLargo(numCaracteres){
+    if(numCaracteres>=8 && numCaracteres<=16){
+        return true
+    }else{
+        return false
+    }
 }
-alert(password.join(""))
-console.log(password.join())
+
+while(!numeroValido) {    
+	numCaracteres = parseInt( prompt('La contraseña debe tener entre 8 y 16 caracteres, ingresa otro valor.') );
+	numeroValido = validarLargo(numCaracteres)
 }
+
+// function randomPass(numCaracteres){
+for(let i=1;i<=numCaracteres;i++){
+    let min = 33;
+    let max = 122;
+const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+password.push(String.fromCharCode(randomNumber))
+}
+// String.fromCharCode()
+alert("Su contraseña es: "+password.join(""))
+// }
+
+
+
 
